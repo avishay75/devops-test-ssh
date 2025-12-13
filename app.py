e5c25c179e33cce662e00a6d47fbc505c9172a5b
Avishay <avishaygbre75@gmail.com>
2025-12-13 13:40:56 +0200

    update app.py (avishay gabara)

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 print("Mayo Club:")
 count=int(input("how many people:"))
+number=0
 counter=0
 list=""
 for i in range(count):
   counter+=1
   a=input("name:")
   b=input("last name:")
-  print(f"name:{a}")
-  print(f"last name:{b}")
-  list+=f"full name:{a}" "{b}"
-print(list)
-print("people:{counter}")
+  print(f"person:{counter}")
+  print(f"full name:{a} {b}")
+  print()
+  list+=f"{counter}.full name:{a} {b}"
+  if counter!=count:
+     list+="\n"
+print(f"List of person:\n{list}\npeople:{counter}")
+
