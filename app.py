f9000a912dc91f840e116035c6ab25327ac0f2a2
avishay75 <avishaygbre75@gmail.com>
2025-12-13 13:06:16 +0200

    Update app.py
    Added functionality to input names and count people.

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+print("Mayo Club:")
 count=int(input("how many people:"))
+counter=0
+list=""
 for i in range(count):
+  counter+=1
   a=input("name:")
   b=input("last name:")
   print(f"name:{a}")
   print(f"last name:{b}")
+  list+=f"full name:{a}" "{b}"
+print(list)
+print("people:{counter}")
